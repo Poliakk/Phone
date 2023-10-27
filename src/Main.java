1e3b2aadfe08643c694d11c092cc96cf60e5b77b
Julia Default GITHUB Profile <poliakovajv@gmail.com>
2023-10-27 22:55:12 +0300

    DZ5_lesson6_2 создана новая ветвь training. Измененное дз5 записано в неё.

--- src/Main.java
+++ src/Main.java
@@ -11,22 +11,31 @@
 public class Main {
     public static void main(String[] args) {
         Phone tel1 = new Phone();
-        tel1.setNumber("+7(910)555-44-33");
+        tel1.setNumber("+7(910)555-44-33"); //private number только через сеттер, package-private setNumber
+        /*лучше сеттеры? в них м.впихнуть проверки данных
+        tel1.model = "Honor";               //package-private model
+        tel1.weight = 320;                  //protected weight
+         */
         tel1.setModel("Honor");
         tel1.setWeight(320);
         tel1.info();
 
-        Phone tel2 = new Phone("+7(900)111-11-11", "Taiga", 1500);
+        Phone tel2 = new Phone("+7(900)111-11-11", "Taiga");
+        tel2.setWeight(1500);
         tel2.info();
 
         Phone tel3 = new Phone("+7(950)333-11-77", "Nokia", 250);
         tel3.info();
 
-        tel1.receiveCall("Alex");
+        /* длинно
+        tel1.receiveCall_0("Alex");
         System.out.println(tel1.getNumber());
+        //через number
+        tel1.receiveCall_1("Alex");
+         */
+        tel1.receiveCall("Alex");
         tel2.receiveCall("Nick");
-        System.out.println(tel2.getNumber());
-        tel3.receiveCall("Kate");
-        System.out.println(tel3.getNumber());
+        tel3.receiveCall("Kate", tel3.getNumber()); //private number
+        //System.out.println(tel3.getNumber());
     }
 }
--- src/Phone.java
+++ src/Phone.java
@@ -1,10 +1,10 @@
 public class Phone {
-    protected String number, model;
+    private String number;
+    String model;    //package-private
     protected int weight;
 
     public Phone() {
     }
-
     public Phone(String number, String model) {
         this.number = number;
         this.model = model;
@@ -14,27 +14,44 @@ public class Phone {
         this.model = model;
         this.weight = weight;
     }
-    public void receiveCall(String callersName) {
-        System.out.println("\nCall from " + callersName);
+    /*длинно
+    public void receiveCall_0(String callersName) {
+        System.out.println("\nCall from " + callersName +"\n");
     }
+    public void receiveCall_1(String callersName) {
+        System.out.println("\nCall from " + callersName + "\n" + number);  //работает с private
+    }
+    */
+    public void receiveCall(String callersName) {
+        System.out.println("\nCall from " + callersName + "\n" + getNumber());
+    }
+    public void receiveCall(String callersName, String num) {
+        System.out.println("\nCall from " + callersName + "\n" + num);
+    }
+
     public String getNumber() {
         return number;
     }
-    public void setNumber(String number) {
+    void setNumber(String number) { //предупр-е, т.к. setNumber не public
         this.number = number;
     }
     public String getModel() {
         return model;
     }
-    public void setModel(String model) {
+    void setModel(String model) {//предупр-е, т.к. setModel не public
         this.model = model;
     }
     public int getWeight() {
         return weight;
     }
-    public void setWeight(int weight) {
+    void setWeight(int weight) {
         this.weight = weight;
     }
+    /*работает с private number
+    public void info_0() {
+        System.out.printf("Phone number: %s\nmodel: %s\tweight: %dг\n", number, getModel(), getWeight());
+    }
+     */
     public void info() {
         System.out.printf("Phone number: %s\nmodel: %s\tweight: %dг\n", getNumber(), getModel(), getWeight());
     }
