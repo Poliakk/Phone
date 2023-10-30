2b32e995f531a1e8196f84732e4ff79850142caa
Julia Default GITHUB Profile <poliakovajv@gmail.com>
2023-10-30 03:07:43 +0300

    Dz5_training_3 с id.

--- src/Phone.java
+++ src/Phone.java
@@ -1,40 +1,61 @@
 public class Phone {
+    private static int counter;
     private String number, model;
     private int weight;
+    private int id;
+
+    {
+        counter++;
+        id = counter;
+    }
 
     public Phone() {
     }
+
     public Phone(String number, String model) {
         this.number = number;
         this.model = model;
     }
+
     public Phone(String number, String model, int weight) {
         this.number = number;
         this.model = model;
         this.weight = weight;
     }
+
+    protected static int getCounter() {
+        return counter;
+    }
+
     public void receiveCall(String callersName) {
         System.out.println("\nCall from " + callersName + "\n" + getNumber());
     }
-    public String getNumber() {
+
+    protected String getNumber() {
         return number;
     }
+
     protected void setNumber(String number) {
         this.number = number;
     }
-    public String getModel() {
+
+    protected String getModel() {
         return model;
     }
+
     protected void setModel(String model) {//предупр-е, т.к. setModel не public
         this.model = model;
     }
-    public int getWeight() {
+
+    protected int getWeight() {
         return weight;
     }
+
     protected void setWeight(int weight) {
         this.weight = weight;
     }
-    public void info() {
-        System.out.printf("Phone number: %s\nmodel: %s\tweight: %dг\n", getNumber(), getModel(), getWeight());
+
+    void info() {
+        System.out.printf("ID: %d\tPhone number: %s\nmodel: %s\tweight: %dг\n", id, number, model, weight);
     }
 }
